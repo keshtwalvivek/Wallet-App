c0d5f380f24df3b02b0998bf5197af06c7b9b351
vivekkeshtwal09@gmail.com <116489124+keshtwalvivek@users.noreply.github.com>
2026-03-25 19:47:19 +0530

    Update Api url

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,2 +1,1 @@
-// export const API_URL = "https://wallet-api-cxqp.onrender.com/api";
-export const API_URL = "http://10.232.65.142:5001/api";
+export const API_URL = "https://wallet-app-x9wx.onrender.com/api";
